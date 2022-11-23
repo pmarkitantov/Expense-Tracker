d9e869e4e7e8ce033d4f653c66bb5c9cffd390c0
Pavel Markitantov <pmarkitantov@gmail.com>
2022-11-24 00:44:46 +0100

    Initial Commit

---
+++ Expense Tracker/Expense_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_TrackerApp.swift
+//  Expense Tracker
+//
+//  Created by Pavel Markitantov on 24/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct Expense_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
